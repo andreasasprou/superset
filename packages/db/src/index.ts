f74892211a965e8d648cadd731383cee72f48b4a
Kiet Ho <hoakiet98@gmail.com>
2025-10-26 20:35:26 -0700

    typecheck

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,4 @@
 export { db } from "./client";
 export type { DrizzleDb } from "./client";
-export * from "./defaults";
-export * from "./mappers";
 export * from "./schema";
 export * from "./seed/constants";
